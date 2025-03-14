0b1d693d3690b1093e1346dbd103c5af7d33c0e3
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2025-03-14 21:06:10 +0100

    Add main file and BankAccount class

---
+++ app/src/main/java/com/example/bankaccountprogram/BankAccount.kt
@@ -0,0 +1,19 @@
+package com.example.bankaccountprogram
+
+class BankAccount(var accountHolder:String, var balance:Double)
+{
+
+    private val _transactionHistory= mutableListOf<String>();
+    fun deposit(amount:Double)
+    {
+
+    }
+    fun withdraw(amount:Double)
+    {
+
+    }
+    fun displayTransactionHistory()
+    {
+
+    }
+}

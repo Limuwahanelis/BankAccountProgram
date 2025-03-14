4ef89c5cb86579b07c5b6e67f3c426bba354fd6b
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2025-03-14 21:21:27 +0100

    Implement functions.

--- app/src/main/java/com/example/bankaccountprogram/BankAccount.kt
+++ app/src/main/java/com/example/bankaccountprogram/BankAccount.kt
@@ -6,14 +6,24 @@ class BankAccount(var accountHolder:String, var balance:Double)
     private val _transactionHistory= mutableListOf<String>();
     fun deposit(amount:Double)
     {
-
+        balance+=amount;
+        _transactionHistory.add("$accountHolder deposited $$amount");
     }
     fun withdraw(amount:Double)
     {
-
+        if(amount<=balance)
+        {
+            balance -= amount;
+            _transactionHistory.add("$accountHolder withdrawn $$amount");
+        }
+        else println("Not enough money to withdraw $amount");
     }
     fun displayTransactionHistory()
     {
-
+        println("Transaction history for $accountHolder");
+        for (transaction in _transactionHistory)
+        {
+            println(transaction);
+        }
     }
 }
--- app/src/main/java/com/example/bankaccountprogram/BankMain.kt
+++ app/src/main/java/com/example/bankaccountprogram/BankMain.kt
@@ -2,5 +2,11 @@ package com.example.bankaccountprogram
 
 fun main()
 {
+    val denisBankAccount = BankAccount("Denis gds", 1294.00);
 
+    println(denisBankAccount.accountHolder);
+
+    denisBankAccount.deposit(41.89);
+    denisBankAccount.withdraw(150.55);
+    denisBankAccount.displayTransactionHistory();
 }
